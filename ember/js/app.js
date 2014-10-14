8144435d9064d89d6c8f63ab8a576db3473a9f8a
Tom Gardham-Pallister <thomasgardham@googlemail.com>
2014-10-14 18:08:01 +0100

    redirect handler

---
+++ ember/js/app.js
@@ -0,0 +1,44 @@
+App = Ember.Application.create({
+    // Basic logging, e.g. "Transitioned into 'post'"
+  LOG_TRANSITIONS: true, 
+
+  // Extremely detailed logging, highlighting every internal
+  // step made while transitioning into a route, including
+  // `beforeModel`, `model`, and `afterModel` hooks, and
+  // information about redirects and aborted transitions
+  LOG_TRANSITIONS_INTERNAL: true,
+  LOG_ACTIVE_GENERATION: true
+});
+
+App.Router.map(function() {
+    this.resource('resetpassword', {path: '/resetpassword/:aggregateId/:resetPasswordId' });
+    this.resource('index', { path: '/' });
+});
+
+
+App.ResetpasswordView = Ember.View.extend({
+  didInsertElement: function() {   
+  }
+});
+
+App.ResetpasswordRoute = Ember.Route.extend({  
+    beforeModel: function() {
+    },
+
+    setupController: function(controller, data) {
+      var isChrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
+      if(isChrome){
+        var w = (window.parent)?window.parent:window
+        w.location.assign('firstoneto://resetpassword/' + data.aggregateId + '/' + data.resetPasswordId);
+      } else {
+        javascript:document.location = 'firstoneto://resetpassword/' + data.aggregateId + '/' + data.resetPasswordId;
+      }
+    }
+});
+
+App.ResetpasswordController = Ember.Controller.extend({
+  
+    init: function () {
+    this._super();
+    },
+});
